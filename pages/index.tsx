3de62f9c4b4e41e8c4520fa83ba60509cc6d13f3
dancingKimDH <kimdonghyun0701@gmail.com>
2024-01-21 17:40:32 +0900

    index 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,17 +26,6 @@ export default function Home() {
 
 
   return (
-    //   {/* _app.tsx에서 Layout 추가 */}
-    //   <div className="font-bold">Map Index Page</div>
-    //   <ul className="px-4 py-2">
-    //     <li><Link href="/stores">맛집 목록</Link></li>
-    //     <li><Link href="/stores/new">맛집 생성</Link></li>
-    //     <li><Link href="/stores/1">맛집 상세 페이지</Link></li>
-    //     <li><Link href="/stores/1/edit">맛집 수정</Link></li>
-    //     <li><Link href="/users/login">로그인</Link></li>
-    //     <li><Link href="/users/mypage">마이페이지</Link></li>
-    //     <li><Link href="/users/likes">찜한 맛집</Link></li>
-    //   </ul>
     <>
     <Script strategy="afterInteractive" type="text/javascript" onReady={loadKakaoMap}
      src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}/>
