1fecfdc5ef765b52ef5f14ea373002be1912daf0
dancingKimDH <kimdonghyun0701@gmail.com>
2024-01-22 11:27:25 +0900

    restaurant detail

--- components/StoreBox.tsx
+++ components/StoreBox.tsx
@@ -1,5 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
+import { AiOutlineCheck, AiOutlineClose, AiOutlineInfoCircle, AiOutlinePhone } from "react-icons/ai";
+import { HiOutlineMapPin } from "react-icons/hi2";
 
 interface StoreBoxProps {
     store: any;
@@ -11,10 +13,40 @@ export default function StoreBox({ store, setStore }) {
         <>
             <div className="fixed transition ease-in-out delay-150 inset-x-0 mx-auto bottom-20 rounded-lg shadow-lg max-w-sm md:max-w-xl z-10 w-full bg-white">
                 {store && (
-                    <div>
-                        <Image width={50} height={50} alt="아이콘 이미지"
-                        src={store?.bizcnd_code_nm ? `/images/markers/${store?.bizcnd_code_nm}.png` : "/images/markers/default.png"} />
-                    </div>
+                    <>
+                        <div className="p-8 ">
+                            <div className="flex justify-between items-start">
+                                <div className="flex gap-4 items-center">
+                                    <Image width={40} height={40} alt="아이콘 이미지"
+                                        src={store?.bizcnd_code_nm ? `/images/markers/${store?.bizcnd_code_nm}.png` : "/images/markers/default.png"} />
+                                    <div>
+                                        <div className="font-semibold">{store?.upso_nm}</div>
+                                        <div className="text-sm">{store?.cob_code_nm}</div>
+                                    </div>
+                                </div>
+                                <button type="button" onClick={() => setStore(null)}><AiOutlineClose /></button>
+                            </div>
+                            <div className="mt-2 flex gap-2 items-center">
+                                <HiOutlineMapPin />
+                                {store?.rdn_code_nm}
+                            </div>
+                            <div className="mt-2 flex gap-2 items-center">
+                                <AiOutlinePhone />
+                                {store?.tel_no}
+                            </div>
+                            <div className="mt-2 flex gap-2 items-center">
+                                <AiOutlineInfoCircle />
+                                {store?.crtfc_gbn_nm}
+                            </div>
+                            <div className="mt-2 flex gap-2 items-center">
+                                <AiOutlineCheck />
+                                {store?.bizcnd_code_nm}
+                            </div>
+                        </div>
+                        <button type="button"
+                        className="w-full bg-blue-700 hover:bg-blue-500 focus:bg-blue-500 py-3 text-white font-semibold rounded-b-lg"
+                        onClick={() => window.alert("작업 중")}>상세 보기</button>
+                    </>
                 )}
             </div>
 
