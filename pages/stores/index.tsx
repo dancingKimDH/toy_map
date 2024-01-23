1f20e62ecbf05f69b070ed565ce02025fddc14b1
dancingKimDH <kimdonghyun0701@gmail.com>
2024-01-23 09:07:39 +0900

    restaurant list up

--- pages/stores/index.tsx
+++ pages/stores/index.tsx
@@ -1,7 +1,36 @@
-export default function StoreListPage() {
+import { StoreType } from "@/interface";
+import Image from "next/image";
+
+export default function StoreListPage({ stores }: { stores: StoreType[] }) {
     return (
-        <div>
-            <h1>Store List</h1>
+        <div className="px-4 md:max-w-4xl mx-auto py-8">
+            <ul role="list" className="divide-y divide-gray-100">
+                {stores?.map((store, index) => (
+                    <li className="flex justify-between gap-x-6 py-5" key={index}>
+                        <div className="flex gap-x-4">
+                            <Image width={48} height={48} alt="image"
+                                src={store?.bizcnd_code_nm ? `/images/markers/${store?.bizcnd_code_nm}.png` : "/images/markers/default.png"} />
+
+                            <div className="">
+                                <div className="text-sm font-semibold leading-6 text-gray-900">
+                                    {store?.upso_nm}
+                                </div>
+                                <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                                    {store?.upso_nm}
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const stores = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/store`).then((res) => res.json());
+
+    return {
+        props: { stores },
+    }
 }
