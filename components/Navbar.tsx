af4ad094756d54047d720d55eae12e1ac81cfbbb
dancingKimDH <kimdonghyun0701@gmail.com>
2024-01-27 15:48:13 +0900

    login page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import Link from "next/link";
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Navbar() {
 
     const [isOpen, setIsOpen] = useState(false);
-    const {data, status} = useSession();
+    const { data, status } = useSession();
 
     console.log(data, status);
 
@@ -22,8 +22,8 @@ export default function Navbar() {
                     <Link className="navbar__list-item" href="/stores/new">맛집 등록</Link>
                     <Link className="navbar__list-item" href="/stores">맛집 목록</Link>
                     <Link className="navbar__list-item" href="/users/likes">찜한 가게</Link>
-                    
-                    {status === "authenticated" ? <button>로그아웃</button> : <Link className="navbar__list-item" href="/api/auth/signin">로그인</Link>}
+
+                    {status === "authenticated" ? <button onClick={() => signOut()}>로그아웃</button> : <Link className="navbar__list-item" href="/api/auth/signin">로그인</Link>}
 
                 </div>
 
@@ -38,11 +38,11 @@ export default function Navbar() {
             {isOpen && (
                 <div className="navbar-mobile">
                     <div className="navbar__list-mobile">
-                    <Link className="navbar__list-item-mobile" href="/stores">맛집 목록</Link>
-                    <Link className="navbar__list-item-mobile" href="/stores/new">맛집 등록</Link>
-                    <Link className="navbar__list-item-mobile" href="/stores">맛집 목록</Link>
-                    <Link className="navbar__list-item-mobile" href="/users/likes">찜한 가게</Link>
-                    <Link className="navbar__list-item-mobile" href="/api/auth/signin">로그인</Link>
+                        <Link className="navbar__list-item-mobile" href="/stores">맛집 목록</Link>
+                        <Link className="navbar__list-item-mobile" href="/stores/new">맛집 등록</Link>
+                        <Link className="navbar__list-item-mobile" href="/stores">맛집 목록</Link>
+                        <Link className="navbar__list-item-mobile" href="/users/likes">찜한 가게</Link>
+                        <Link className="navbar__list-item-mobile" href="/api/auth/signin">로그인</Link>
                     </div>
                 </div>
             )}
--- pages/users/login.tsx
+++ pages/users/login.tsx
@@ -1,5 +1,44 @@
+import { AiOutlineGoogle } from "react-icons/ai";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { SiNaver } from "react-icons/si";
+
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router"
+
+import { useEffect } from "react";
+
 export default function LoginPage() {
-    return(
-        <div>Login Page</div>
+
+    const { status, data: session } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(status === "authenticated") {
+            router.replace("/")
+        }
+    }, [router, status])
+
+    return (
+        <div className="flex flex-col justify-center px-6 lg:px-8 h-[60vh] mt-[100px]">
+            <div className="mx-auto w-full max-w-sm">
+                <h1 className="text-blue-800 text-center text-2xl font-semibold italic">Map For Foodies</h1>
+            </div>
+            <div className="text-center mt-6 text-2xl font-bold text-gray-600">SNS 계정으로 로그인</div>
+            <div className="mt-2 text-center text-sm text-gray-600">계정이 없다면 자동으로 회원가입이 진행됩니다</div>
+            <div className="mt-10 mx-auto w-full max-w-sm">
+                <div className="flex flex-col gap-3">
+                    <button type="button"
+                        onClick={() => signIn("google")}
+                        className="text-white flex gap-3 bg-[#4285f4] hover:bg-[#4285f4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
+                        <AiOutlineGoogle className="w-6 h-6" />Sign In with Google</button>
+                    <button type="button"
+                        onClick={() => signIn("naver")}
+                        className="text-white flex gap-4 bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
+                        <SiNaver className="w-4 h-6" />Sign In with Naver</button>
+                    <button type="button" className="text-black flex gap-4 bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center">
+                        <RiKakaoTalkFill className="w-6 h-6" />Sign In with Kakao</button>
+                </div>
+            </div>
+        </div>
     )
 }
